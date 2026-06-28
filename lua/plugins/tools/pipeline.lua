5175bee74c93a8c73fe258206d1f58c22a4873ce
Kaboupi <gnovozhiloff@gmail.com>
2026-06-28 18:00:58 +0300

    Add: plugins
    - Octo.nvim (GH, issues, etc.)
    - Pipeline.nvim (CI - GH, GL)

---
+++ lua/plugins/tools/pipeline.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "topaxi/pipeline.nvim",
+    keys = {
+      { "<leader>ci", "<cmd>Pipeline<cr>", desc = "Open pipeline.nvim" },
+    },
+    build = "make",
+    ---@type pipeline.Config
+    opts = {},
+  },
+}
